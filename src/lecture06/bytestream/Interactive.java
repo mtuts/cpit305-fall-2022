bcfb150a0e5436795ddcd3ada315c50531147f63
Yahya <44480217+y7ya@users.noreply.github.com>
2022-09-14 01:12:10 +0300

    Update Interactive.java
    Done.

--- src/lecture06/bytestream/Interactive.java
+++ src/lecture06/bytestream/Interactive.java
@@ -4,6 +4,22 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Scanner;
 
+/**
+ * new Methods:
+ *      nameMatch(), idExist(), deleteEmployee(), searchForEmployeeByID()
+ * 
+ * - Done TODO: create method nameMatch() to check if stored name equals given name
+ * - Done TODO: create method idExist() to check if id exist before add or edit user
+ * - Done TODO: listUsersWithGivenName() print All users with the given name
+ * - Done TODO: searchForEmployeeByID() Get Employee By ID
+ * - Done TODO: deleteEmployee() replce last Employee with deleted Employee 
+ * - Done TODO: delete empty bytes at the end of file
+ * 
+ * Note:
+ *     * You can't manage the employee when search by name.
+ *
+ */
+
 public class Interactive {
 
     static final int ID_LENGTH = 4;
@@ -20,8 +36,9 @@ public class Interactive {
             System.out.println("\nWelcome to our mini-database system\n");
             System.out.println("1. Add new Employee");
             System.out.println("2. Get Employee by record #");
-            System.out.println("3. Search for by Empolyee name");
-            System.out.println("4. List all Employees");
+            System.out.println("3. Get Employee by ID #");
+            System.out.println("4. List Employees by name");
+            System.out.println("5. List all Employees");
             System.out.println("10. Exit");
             System.out.print("Choose from above: ");
 
@@ -35,7 +52,7 @@ public class Interactive {
                 int record_number = Integer.parseInt(option);
                 long pos = (record_number - 1) * RECORD_SIZE;
 
-                if (pos <= raf.length() - RECORD_SIZE) {                
+                if (pos <= raf.length() - RECORD_SIZE) {
                     raf.seek(pos);
 
                     manageRecord(keyboard, raf);
@@ -43,18 +60,28 @@ public class Interactive {
                     System.out.printf("The record (%d) is not exist\n", record_number);
                 }
             } else if (option.equals("3")) {
+
+                System.out.print("Enter employee ID to search for it: ");
+                int emp_id = Integer.parseInt(keyboard.nextLine());
+                int record_num = searchForEmployeeByID(emp_id, raf);
+                if (record_num < 1) {
+                    System.out.printf("(%s) not in data file!\n", emp_id);
+                    continue;
+                }
+                manageRecord(keyboard, raf);
+
+            } else if (option.equals("4")) { 
+
                 System.out.print("Enter employee name to search for it: ");
                 String emp_name = keyboard.nextLine();
-                int record_num = searchForEmployeeName(emp_name, raf);
-
+                int record_num = listUsersWithGivenName(emp_name, raf);
                 if (record_num < 1) {
                     System.out.printf("(%s) not in data file!\n", emp_name);
                     continue;
                 }
-                manageRecord(keyboard, raf);
-            } else if (option.equals("4")) {
+            } else if (option.equals("5")) {
                 raf.seek(0); // let current point to the begining of file
-                int num_of_records = (int)(raf.length() / RECORD_SIZE);
+                int num_of_records = (int) (raf.length() / RECORD_SIZE);
                 System.out.println();
                 System.out.printf("%-10s | %-20s | %-10s\n", "id", "name", "salary");
                 System.out.println("---------------------------------------------------------");
@@ -72,6 +99,36 @@ public class Interactive {
         raf.close();
     }
 
+    private static int listUsersWithGivenName(String emp_name, RandomAccessFile raf) throws IOException {
+        raf.seek(0);
+        int num_of_records = (int) raf.length() / RECORD_SIZE;
+        int found = 0;
+        long pos;
+
+        for (int i = 0; i < num_of_records; i++) {
+
+            raf.skipBytes(ID_LENGTH);
+            String name = readFixedString(raf); // new pos = 4 + 15 = 19
+            // if (name.toLowerCase().contains(emp_name.toLowerCase())) {
+            if (nameMatch(name, emp_name)) {
+                raf.seek(raf.getChannel().position() - ID_LENGTH - NAME_LENGTH);
+                if (found == 0) {
+                    System.out.printf("\n%-10s | %-20s | %-10s\n", "id", "name", "salary");
+                    System.out.println("---------------------------------------------------------");
+                }
+
+                pos = raf.getChannel().position();
+                displayEmployee(raf);
+                raf.seek(pos - SALARY_LENGTH + RECORD_SIZE);
+                found++;
+            }
+            // move to next record
+            raf.skipBytes(SALARY_LENGTH); // skip salary of current record and skip id of next one
+        }
+
+        return found;
+    }
+
     private static void manageRecord(Scanner keyboard, RandomAccessFile raf) throws IOException {
         System.out.println("\n------------------");
         displayEmployee(raf);
@@ -80,53 +137,50 @@ public class Interactive {
 
         System.out.println("1. Edit");
         System.out.println("2. Delete (Not implemented yet)");
-        
+
         String option = keyboard.nextLine();
 
         if (option.equals("1")) {
             editEmployee(keyboard, raf);
         } else if (option.equals("2")) {
-            System.out.println("Not implemented yet");
+            deleteEmployee(raf);
         } else {
             System.out.println("Wrong choice!");
         }
     }
 
-    private static int searchForEmployeeName(String emp_name, RandomAccessFile raf) throws IOException {
+    private static void deleteEmployee(RandomAccessFile raf) throws IOException {
+        long pos = raf.getChannel().position();
+        raf.seek(raf.length() - RECORD_SIZE);
 
-        // testing first and second record
-        // raf.seek(ID_LENGTH);
-        // String name = readFixedString(raf);  // new pos = 4 + 15 = 19
-        // if (name.toLowerCase().contains(emp_name.toLowerCase())) {
-        //     raf.seek(raf.getChannel().position() - 4 - NAME_LENGTH);
-        //     System.out.println("We found it, choose from following: ");
-        //     return (int)raf.getChannel().position() / RECORD_SIZE + 1;
-        // }
-        // // move to next record
-        // raf.skipBytes(SALARY_LENGTH + ID_LENGTH); // skip salary of current record and skip id of next one
-        
-        // name = readFixedString(raf);  // new pos = 4 + 15 = 19
-        // if (name.toLowerCase().contains(emp_name.toLowerCase())) {
-        //     raf.seek(raf.getChannel().position() - 4 - NAME_LENGTH);
-        //     System.out.println("We found it, choose from following: ");
-        //     return (int)raf.getChannel().position() / RECORD_SIZE + 1;
-        // }
+        int id = raf.readInt();
+        String name = readFixedString(raf);
+        double salary = raf.readDouble();
 
+        raf.setLength(raf.length() - RECORD_SIZE);
 
-        // General solution for all records
+        raf.seek(pos);
+        raf.writeInt(id);
+        writeFixedString(name, raf);
+        raf.writeDouble(salary);
+        System.out.println("Employee record has been deleted");
+    }
+
+    private static int searchForEmployeeByID(int emp_id, RandomAccessFile raf) throws IOException {
+
         raf.seek(0);
-        int num_of_records = (int)raf.length() / RECORD_SIZE;
+        int num_of_records = (int) raf.length() / RECORD_SIZE;
         for (int i = 0; i < num_of_records; i++) {
-            
-            raf.skipBytes(ID_LENGTH);
-            String name = readFixedString(raf);  // new pos = 4 + 15 = 19
-            if (name.toLowerCase().contains(emp_name.toLowerCase())) {
-                raf.seek(raf.getChannel().position() - 4 - NAME_LENGTH);
+
+            int id = raf.readInt();
+
+            if (id == emp_id) {
+                raf.seek(raf.getChannel().position() - 4);
                 System.out.println("We found it, choose from following: ");
-                return (int)raf.getChannel().position() / RECORD_SIZE + 1;
+                return (int) raf.getChannel().position() / RECORD_SIZE + 1;
             }
             // move to next record
-            raf.skipBytes(SALARY_LENGTH); // skip salary of current record and skip id of next one
+            raf.skipBytes(NAME_LENGTH + SALARY_LENGTH);
         }
 
         return 0;
@@ -135,10 +189,13 @@ public class Interactive {
     private static void addNewEmployee(Scanner keyboard, RandomAccessFile raf) throws IOException {
         System.out.print("Enter employee ID: ");
         int id = Integer.parseInt(keyboard.nextLine());
-        
+        if (idExist(raf, id)) {
+            System.out.printf("[%d] is already Exist", id);
+            return;
+        }
         System.out.print("Enter employee Name: ");
         String name = keyboard.nextLine();
-        
+
         System.out.print("Enter employee Salary: ");
         double salary = Double.parseDouble(keyboard.nextLine());
 
@@ -148,6 +205,16 @@ public class Interactive {
         writeFixedString(name, raf);
         raf.writeDouble(salary);
 
+    }
+
+    private static boolean idExist(RandomAccessFile raf, int id) throws IOException {
+        raf.seek(0);
+        for (int i = 0; i < (raf.length() / RECORD_SIZE); i++) {
+            if (raf.readInt() == id)
+                return true;
+            raf.skipBytes(NAME_LENGTH + SALARY_LENGTH);
+        }
+        return false;
     }
 
     private static void writeFixedString(String name, RandomAccessFile raf) throws IOException {
@@ -169,7 +236,7 @@ public class Interactive {
         System.out.println("1. ID");
         System.out.println("2. Name");
         System.out.println("3. Salary");
-        
+
         String option = keyboard.nextLine();
 
         long pos = raf.getChannel().position();
@@ -177,17 +244,21 @@ public class Interactive {
         if (option.equals("1")) {
             System.out.print("Enter Employee new ID");
             int id = Integer.parseInt(keyboard.nextLine());
+            if (idExist(raf, id)) {
+                System.out.printf("\n[%d] is already Exist", id);
+                return;
+            }
             raf.writeInt(id);
         } else if (option.equals("2")) {
             System.out.print("Enter Employee new Name");
             String name = keyboard.nextLine();
-            
+
             raf.skipBytes(ID_LENGTH);
             writeFixedString(name, raf);
         } else if (option.equals("3")) {
             System.out.print("Enter Employee new Salary");
             double sal = Double.parseDouble(keyboard.nextLine());
-            
+
             raf.skipBytes(ID_LENGTH + NAME_LENGTH);
             raf.writeDouble(sal);
         } else {
@@ -205,7 +276,7 @@ public class Interactive {
         double salary = raf.readDouble();
         System.out.printf("%-10d | %-20s | %-10.2f\n", id, name, salary);
     }
-    
+
     public static String readFixedString(RandomAccessFile raf) throws IOException {
         String str = "";
         byte[] buff = new byte[NAME_LENGTH];
@@ -213,10 +284,20 @@ public class Interactive {
         raf.read(buff);
 
         for (int i = 0; i < buff.length; i++) {
-            if (buff[i] == 0) break;
+            if (buff[i] == 0)
+                break;
             str += (char) buff[i];
         }
 
         return str;
+    }
+
+    private static boolean nameMatch(String fullName, String givenName) {
+        String[] splited = fullName.split(" ");
+        for (String s : splited) {
+            if (s.equalsIgnoreCase(givenName))
+                return true;
+        }
+        return false;
     }
 }
